495b594bf8e6cd3ff2569ee59b89f2523f17f3ad
shivansh <shivansh4858@gmail.com>
2025-04-14 11:39:42 +0530

    base url changed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import {BrowserRouter} from "react-router-dom"
 import axios from 'axios';
 import App from './App.jsx'
 
-axios.defaults.baseURL = 'http://localhost:8080/api/v1';
+axios.defaults.baseURL = 'https://skill-mate.onrender.com/api/v1';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
