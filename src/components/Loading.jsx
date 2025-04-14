0bb2ae03f89d2617ac308c231d4aff0db3b36106
shivansh <shivansh4858@gmail.com>
2025-04-14 14:31:43 +0530

    loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="flex justify-center items-center ml-4">
+            <div className="w-8 h-8 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+        </div>
+    );
+};
+
+export default Loading;
