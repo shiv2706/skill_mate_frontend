4cae7e3524aa957d896cfcc6e44c62f1d7baf208
shivansh <shivansh4858@gmail.com>
2025-04-23 03:37:26 +0530

    view my applications status

--- src/pages/YourProfile.jsx
+++ src/pages/YourProfile.jsx
@@ -831,46 +831,46 @@ const YourProfile = ()=> {
                                         <div className="flex flex-none items-center gap-x-4">
                                             <button disabled={notActive}
                                                 onClick={() => WithdrawApplicationHandler(project.applicantProfileId, project.appliedFor)}
-                                                className="hidden rounded-md bg-white cursor-pointer px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-red-500 sm:block"
+                                                className="rounded-md bg-white cursor-pointer px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-red-500 sm:block"
                                             >
                                                 Withdraw
                                             </button>
-                                            <Menu as="div" className="relative flex-none">
-                                                <MenuButton
-                                                    className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
-                                                    <span className="sr-only">Open options</span>
-                                                    <EllipsisVerticalIcon aria-hidden="true" className="size-5"/>
-                                                </MenuButton>
-                                                <MenuItems
-                                                    transition
-                                                    className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-                                                >
-                                                    <MenuItem>
-                                                        <a
-                                                            href="#"
-                                                            className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"
-                                                        >
-                                                            Edit<span className="sr-only">, {project.name}</span>
-                                                        </a>
-                                                    </MenuItem>
-                                                    <MenuItem>
-                                                        <a
-                                                            href="#"
-                                                            className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"
-                                                        >
-                                                            Move<span className="sr-only">, {project.name}</span>
-                                                        </a>
-                                                    </MenuItem>
-                                                    <MenuItem>
-                                                        <a
-                                                            href="#"
-                                                            className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"
-                                                        >
-                                                            Delete<span className="sr-only">, {project.name}</span>
-                                                        </a>
-                                                    </MenuItem>
-                                                </MenuItems>
-                                            </Menu>
+                                            {/*<Menu as="div" className="relative flex-none">*/}
+                                            {/*    <MenuButton*/}
+                                            {/*        className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">*/}
+                                            {/*        <span className="sr-only">Open options</span>*/}
+                                            {/*        <EllipsisVerticalIcon aria-hidden="true" className="size-5"/>*/}
+                                            {/*    </MenuButton>*/}
+                                            {/*    <MenuItems*/}
+                                            {/*        transition*/}
+                                            {/*        className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"*/}
+                                            {/*    >*/}
+                                            {/*        <MenuItem>*/}
+                                            {/*            <a*/}
+                                            {/*                href="#"*/}
+                                            {/*                className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"*/}
+                                            {/*            >*/}
+                                            {/*                Edit<span className="sr-only">, {project.name}</span>*/}
+                                            {/*            </a>*/}
+                                            {/*        </MenuItem>*/}
+                                            {/*        <MenuItem>*/}
+                                            {/*            <a*/}
+                                            {/*                href="#"*/}
+                                            {/*                className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"*/}
+                                            {/*            >*/}
+                                            {/*                Move<span className="sr-only">, {project.name}</span>*/}
+                                            {/*            </a>*/}
+                                            {/*        </MenuItem>*/}
+                                            {/*        <MenuItem>*/}
+                                            {/*            <a*/}
+                                            {/*                href="#"*/}
+                                            {/*                className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"*/}
+                                            {/*            >*/}
+                                            {/*                Delete<span className="sr-only">, {project.name}</span>*/}
+                                            {/*            </a>*/}
+                                            {/*        </MenuItem>*/}
+                                            {/*    </MenuItems>*/}
+                                            {/*</Menu>*/}
                                         </div>
                                     </li>
                                 ))}
