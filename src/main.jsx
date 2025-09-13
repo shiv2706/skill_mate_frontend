3306688303aca78c185baad01bee458dd6283ead
shivansh <shivansh4858@gmail.com>
2025-09-13 16:28:51 +0530

    new backend

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 //axios.defaults.baseURL = 'http://localhost:8080/api/v1';
 // axios.defaults.baseURL = 'https://skill-mate.onrender.com/api/v1';
-axios.defaults.baseURL = 'http://skillmate-env.eba-3eqmq2xb.ap-south-1.elasticbeanstalk.com/api/v1';
+axios.defaults.baseURL = 'https://skillmate-env.eba-3eqmq2xb.ap-south-1.elasticbeanstalk.com/api/v1';
 
 
 createRoot(document.getElementById('root')).render(
