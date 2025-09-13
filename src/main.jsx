12625a71e153568fb50de83330ccf917a785e346
shivansh <shivansh4858@gmail.com>
2025-09-13 16:21:56 +0530

    new backend

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import {BrowserRouter} from "react-router-dom"
 import axios from 'axios';
 import App from './App.jsx'
 
-axios.defaults.baseURL = 'https://skill-mate.onrender.com/api/v1';
+
+//axios.defaults.baseURL = 'http://localhost:8080/api/v1';
+// axios.defaults.baseURL = 'https://skill-mate.onrender.com/api/v1';
+axios.defaults.baseURL = 'https://skillmate-env.eba-3eqmq2xb.ap-south-1.elasticbeanstalk.com/api/v1';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
